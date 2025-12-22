9c8d497aa4b0a9fc879bdffb12416431f99b737a
Mano M <154455165+Mano1151@users.noreply.github.com>
2025-12-22 18:13:58 +0530

    Create index.js

---
+++ Backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Todo backend running in Docker/K8s" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
